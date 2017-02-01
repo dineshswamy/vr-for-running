22ac6d69d7da97dd896a790d6fa0eb9368571c6e
dineshswamy <dineshswamy92@gmail.com>
2017-02-01 14:28:02 +0530

    initialized with git

---
+++ Assets/ExplorerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ExplorerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		String speedStr = PlayerPrefs.GetString("speed");
+		int speed = System.Int32.Parse (speedStr);
+		Vector3 input = new Vector3 (0, 0, 1);
+		Vector3 direction = input.normalized;
+		Vector3 velocity = direction * speed;
+		Vector3 moveAmount = velocity * Time.deltaTime;
+		transform.position += moveAmount;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("You are going to be reset");
+		Vector3 resetposition = new Vector3 (230, 11, 10.9f);
+		transform.position = resetposition;
+	}
+}
---
+++ Assets/SetSpeed.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetSpeed : MonoBehaviour {
+
+	// Use this for initialization
+	public void getTreadmillSpeed () {
+		GameObject inputFieldGo = GameObject.Find("InputField");
+		InputField inputFieldCo = inputFieldGo.GetComponent<InputField>();
+		PlayerPrefs.SetString("speed", inputFieldCo.text);
+		//Application.LoadLevel ("Scene2");
+		Debug.Log(inputFieldCo.text);
+	}
+
+}
